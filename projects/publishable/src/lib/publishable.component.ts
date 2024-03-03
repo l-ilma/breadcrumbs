49e237633fbae22e4184297da0259183eab0fb98
Ilma Lušija <ilma.lusija@student.tugraz.at>
2024-03-03 21:23:36 +0100

    add publish lib job

---
+++ projects/publishable/src/lib/publishable.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-publishable',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      publishable works!
+    </p>
+  `,
+  styles: ``
+})
+export class PublishableComponent {
+
+}
---
+++ projects/publishable/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of publishable
+ */
+
+export * from './lib/publishable.service';
+export * from './lib/publishable.component';
